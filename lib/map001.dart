565db057c020001edc55231bd39caa672e196e31
drtun <drtun@live.com>
2024-08-04 07:52:14 +0630

    Remove URL launcher

--- lib/map001.dart
+++ lib/map001.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:provider/provider.dart';
-import 'package:url_launcher/url_launcher.dart'; 
 import 'helpers.dart'; 
 import 'geodata.dart';
 //  -------------------------------------    Map001 (Property of Nirvasoft.com)
